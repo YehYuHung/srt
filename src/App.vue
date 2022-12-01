0598fac25015818159c1168e8a448942a1473ebb
morga.yeh <z123z14795@gmail.com>
2022-12-01 11:50:42 +0800

    [Vue] Mor-模擬表單發送/Vue物件取得方法

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,37 @@
 <template>
-  <div id="app">
-    <!--<img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <ReportView msg="Yoloshiku"/>
-  </div>
+    <div id="app">
+        <!--<img alt="Vue logo" src="./assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App"/> 
+        <h1>{{message}}</h1> -->
+        <ReportView msg="Yoloshiku" />
+    </div>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue';
-import ReportView from './components/ReportView.vue';
+    //import HelloWorld from './components/HelloWorld.vue';
+    import ReportView from './components/ReportView.vue';
 
-export default {
-  name: 'App',
-  components: {
-      //HelloWorld,
-      ReportView
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            //HelloWorld,
+            ReportView
+        },
+        data() {
+            return {
+                message: 'Welcome to Vue!'
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/components/ReportView.vue
+++ src/components/ReportView.vue
@@ -1,7 +1,18 @@
 <template>
-	<div>
+    <div>
         <p>{{ msg }}</p>  <!-- 6. Rerender View -->
-        <!--<button v-on:click="getRemoteMessage">Inside Button</button>   1. Click Button -->
+        <form>
+            <h3>表單測試</h3>
+            <div class="div_text">
+                <label for="username">帳號</label>
+                <input type="text" v-model="username"/>
+            </div>
+            <div class="div_text">
+                <label for="username">密碼</label>
+                <input type="text" v-model="password"/>
+            </div>
+            <button v-on:click="handleOut">送出</button>
+        </form>
         <button v-on:click="clickButton">Outside Button</button>   <!-- 1. Click Button -->
     </div>
 </template>
@@ -9,60 +20,72 @@
 <script>
     import axios from 'axios';
 
-export default {
-  name: "ReportView",
-  props: {
-    msg: String
-  },
-  methods:{
-    clickButton : function(){
-        axios(
-                {
-                    url: 'https://srtserver-production.up.railway.app/excel',
-                    method: 'GET',
-                    responseType: 'blob',
-                })
-                        .then((response) => {
+    export default {
+        name: "ReportView",
+        props: {
+            msg: String
+        },
+        data() {
+            return {
+                username : '',
+                password : '',
+            }
+        },
+        methods: {
+            handleOut() {
+                alert(`輸入帳號 : ${this.username}\n輸入密碼 : ${this.password}`);
+            },
+            clickButton() {
+                axios(
+                    {
+                        url: 'https://srtserver-production.up.railway.app/excel',
+                        method: 'GET',
+                        responseType: 'blob',
+                    })
+                    .then((response) => {
 
-    var fileNameList = response.headers['content-disposition'].split("filename=");
-    var fileName = fileNameList.length > 1? decodeURIComponent(fileNameList[1]): new Date().toLocaleString().split(' ')[0] + '_test.xlsx'
-    // create file link in browser's memory
-    const href = URL.createObjectURL(response.data);
+                        var fileNameList = response.headers['content-disposition'].split("filename=");
+                        var fileName = fileNameList.length > 1 ? decodeURIComponent(fileNameList[1]) : new Date().toLocaleString().split(' ')[0] + '_test.xlsx'
+                        // create file link in browser's memory
+                        const href = URL.createObjectURL(response.data);
 
-    // create "a" HTML element with href to file & click
-    const link = document.createElement('a');
-    link.href = href;
-    link.setAttribute('download', fileName); //or any other extension
-    document.body.appendChild(link);
-    link.click();
+                        // create "a" HTML element with href to file & click
+                        const link = document.createElement('a');
+                        link.href = href;
+                        link.setAttribute('download', fileName); //or any other extension
+                        document.body.appendChild(link);
+                        link.click();
 
-    // clean up "a" element & remove ObjectURL
-    document.body.removeChild(link);
-    URL.revokeObjectURL(href);
-                        })
-                        .catch((err) => {
-                            console.log(err);
-                        })
+                        // clean up "a" element & remove ObjectURL
+                        document.body.removeChild(link);
+                        URL.revokeObjectURL(href);
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    })
+            }
+        }
     }
-  }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
 </style>
 
